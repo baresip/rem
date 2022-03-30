d4babfce8a9ba4d5d7a9e6771625c53bd17fa38a
Christian Spielberger <c.spielberger@commend.com>
2022-03-30 11:17:15 +0200

    aubuf: remove unused function ajb_bufmin

--- src/aubuf/ajb.h
+++ src/aubuf/ajb.h
@@ -19,4 +19,3 @@ void ajb_calc(struct ajb *ajb, struct auframe *af, size_t sampc);
 enum ajb_state ajb_get(struct ajb *ajb, struct auframe *af);
 int32_t ajb_debug(const struct ajb *ajb);
 void plot_underrun(struct ajb *ajb);
-uint32_t ajb_bufmin(struct ajb *ajb);
