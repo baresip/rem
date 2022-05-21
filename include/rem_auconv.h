a69b08c9a14f5ec536bd818fbd841730e01cd18f
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-05-21 16:58:23 +0200

    auconv: add auconv_to_float() (#53)

--- include/rem_auconv.h
+++ include/rem_auconv.h
@@ -9,3 +9,5 @@ void auconv_from_s16(enum aufmt dst_fmt, void *dst_sampv,
 		     const int16_t *src_sampv, size_t sampc);
 void auconv_to_s16(int16_t *dst_sampv, enum aufmt src_fmt,
 		   void *src_sampv, size_t sampc);
+void auconv_to_float(float *dst_sampv, enum aufmt src_fmt,
+		     const void *src_sampv, size_t sampc);
