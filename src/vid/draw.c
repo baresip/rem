8f29fd7ef61a4537964b51753ec7bd873fab6b83
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-11-01 21:18:25 +0100

    update

--- src/vid/draw.c
+++ src/vid/draw.c
@@ -105,18 +105,6 @@ void vidframe_draw_point(struct vidframe *f, unsigned x, unsigned y,
 		vp[0] = rgb2v(r, g, b);
 		break;
 
-	case VID_FMT_YUYV422:
-
-		uv_offset = (f->linesize[0] * y + x * 2) & ~3;
-
-		yp = f->data[0] + uv_offset;
-
-		yp[0] = rgb2y(r, g, b);
-		yp[1] = rgb2u(r, g, b);
-		yp[2] = rgb2y(r, g, b);
-		yp[3] = rgb2v(r, g, b);
-		break;
-
 	default:
 		(void)re_fprintf(stderr, "vidframe_draw_point:"
 				 " unsupported format %s\n",
